c573e854a3fe64f94688df08ff28d8d82e9a8b9a
Keiju Hiramoto <hiramoto.keiju@gmail.com>
2025-09-14 02:17:35 +0900

    EntryView,LoginView,SignUpViewを作成しました

---
+++ picture_alarm_app/View/Entry/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  picture_alarm_app
+//
+//  Created by Keiju Hiramoto on 2025/09/14.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
