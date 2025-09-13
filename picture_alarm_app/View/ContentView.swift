04cc71a4afceda1c61de2d17f774274c484832dc
a-ru <100020685+aruaru624@users.noreply.github.com>
2025-09-14 01:12:01 +0900

    Revert "Revert "Fix google info.plist""

---
+++ picture_alarm_app/View/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  picture_alarm_app
+//
+//  Created by tanaka niko on 2025/09/12.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+   
+
+    var body: some View {
+
+        TabView {
+            TLView()
+                .tabItem {
+                        Image(systemName: "house")
+                        Text("タイムライン")
+                }
+            AlermView()
+                .tabItem {
+                    Image(systemName: "deskclock")
+                    Text("アラーム")
+                }
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person.crop.circle.fill")
+                    Text("プロフィール")
+                }
+        }
+
+    }
+
+}
+
+#Preview {
+    ContentView()
+
+}
