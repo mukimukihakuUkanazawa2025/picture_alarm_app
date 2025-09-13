0653a9c1a8930829046622fd09f0b2942719de78
a-ru <100020685+aruaru624@users.noreply.github.com>
2025-09-14 01:11:01 +0900

    Revert "Fix google info.plist"

---
+++ picture_alarm_app/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  picture_alarm_app
+//
+//  Created by tanaka niko on 2025/09/12.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+   
+
+    var body: some View {
+        Text("test test")
+    }
+
+}
+
+#Preview {
+    ContentView()
+
+}
--- picture_alarm_app/picture_alarm_appApp.swift
+++ picture_alarm_app/picture_alarm_appApp.swift
@@ -6,28 +6,14 @@
 //
 
 import SwiftUI
-import FirebaseCore
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
-}
 
 @main
-struct YourApp: App {
-  // register app delegate for Firebase setup
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+struct picture_alarm_appApp: App {
 
-
-  var body: some Scene {
-    WindowGroup {
-      NavigationView {
-        ContentView()
-      }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
-  }
 }
