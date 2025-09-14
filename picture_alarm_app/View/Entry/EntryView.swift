6d8d4f04006d00f503817f5e02e8ca9d12abaf7c
Keiju Hiramoto <hiramoto.keiju@gmail.com>
2025-09-14 16:13:42 +0900

    画面遷移修正

--- picture_alarm_app/View/Entry/EntryView.swift
+++ picture_alarm_app/View/Entry/EntryView.swift
@@ -8,63 +8,77 @@
 import SwiftUI
 
 struct EntryView: View {
-    @State var showLoginView: Bool = false
-    @State var showSignUpView: Bool = false
+    @State private var showLoginView: Bool = false
+    @State private var showSignUpView: Bool = false
+    @State private var isSignedUp = false
     
     var body: some View {
-            VStack(spacing: 40) {
-                Spacer()
-                Text("寝顔人質カメラ")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                
-                VStack(spacing: 20) {
-                    Button {
-                        showLoginView = true
-                    } label: {
-                        Text("ログイン")
-                            .font(.headline)
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.orange)
-                            .cornerRadius(12)
+        NavigationStack{
+            ZStack{
+                VStack(spacing: 40) {
+                    Spacer()
+                    Text("寝顔人質カメラ")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
                     
+                    VStack(spacing: 20) {
+                        Button {
+                            showLoginView = true
+                        } label: {
+                            Text("ログイン")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.orange)
+                                .cornerRadius(12)
+                            
+                        }
+                        .fullScreenCover(isPresented: $showLoginView) {
+                            LoginView()
+                        }
+                        
+                        Button {
+                            showSignUpView = true
+                        } label: {
+                            Text("アカウント作成")
+                                .font(.headline)
+                                .foregroundColor(.black)
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.white)
+                                .cornerRadius(12)
+                            //                            .overlay(
+                            //                                RoundedRectangle(cornerRadius: 12)
+                            //                                    .stroke(Color.blue, lineWidth: 2)
+                            //                            )
+                        }
+                        .fullScreenCover(isPresented: $showSignUpView) {
+                            SignUpView(
+                                onSuccess: {
+                                    self.isSignedUp = true
+                                    self.showSignUpView = false
+                                }
+                            )
+                        }
                     }
-                    .fullScreenCover(isPresented: $showLoginView) {
-                        LoginView()
-                    }
+                    .padding(.horizontal, 32)
                     
-                    Button {
-                        showSignUpView = true
-                    } label: {
-                        Text("アカウント作成")
-                            .font(.headline)
-                            .foregroundColor(.black)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(12)
-//                            .overlay(
-//                                RoundedRectangle(cornerRadius: 12)
-//                                    .stroke(Color.blue, lineWidth: 2)
-//                            )
-                    }
-                    .fullScreenCover(isPresented: $showSignUpView) {
-                        SignUpView()
-                    }
+                    Spacer()
                 }
-                .padding(.horizontal, 32)
-                
-                Spacer()
+                .background(.black)
+                .ignoresSafeArea()
             }
-            .background(.black)
-            .ignoresSafeArea()
+            
+            .navigationDestination(isPresented:$isSignedUp){
+                ContentView()
+            }
+            
         }
     }
-
+}
 #Preview {
     EntryView()
 }
