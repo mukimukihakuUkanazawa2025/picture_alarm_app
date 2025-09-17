159af50fc22b37a585691313551540d7186b9bc8
ruiyaa <nini2525min@gmail.com>
2025-09-17 10:20:24 +0900

    自動ログインを実装

---
+++ picture_alarm_app/AuthViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  AuthViewModel.swift
+//  picture_alarm_app
+//
+//  Created by tanaka niko on 2025/09/17.
+//
+
+import Foundation
---
+++ picture_alarm_app/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  picture_alarm_app
+//
+//  Created by tanaka niko on 2025/09/17.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
